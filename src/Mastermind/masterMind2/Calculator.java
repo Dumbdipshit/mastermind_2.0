e1415674a0b8673c5e3a60e497fd883096bed51f
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-01-08 11:39:26 +0100

    Added functions to replace stuff
    Also removed unnecesearry stuff

--- src/Mastermind/masterMind2/Calculator.java
+++ src/Mastermind/masterMind2/Calculator.java
@@ -14,5 +14,22 @@ public class Calculator extends MainGame {
 		return code;
 	}
 	
+	public void check(String[] poging, String code[], int i) {
+		if (poging[i].equals(code[i])) {
+			System.out.println("Positie "+(i+1)+" is goed");
+		}else if(poging[i].equals(code[0])  || poging[i].equals(code[1])|| poging[i].equals(code[2])|| poging[i].equals(code[3])){
+			System.out.println("Positie "+(i+1)+" is bijna goed");
+		}else {
+			System.out.println("Positie "+(i+1)+" is niet goed");
+		}
+	}
+	
+	
+	public void yourInput(String[] poging) {
+		System.out.println("Dit is jouw keuze.");
+		System.out.println("--------------------------");
+		System.out.println(poging[0]+" "+poging[1]+" "+poging[2]+" "+poging[3]);
+	}
 }
+
 
--- src/Mastermind/masterMind2/MainGame.java
+++ src/Mastermind/masterMind2/MainGame.java
@@ -17,11 +17,12 @@ public class MainGame {
 		int rounds = 1;
 		boolean cont = false;
 		
+	
 		//hier genereed de code//	
 		for (int i = 0; i < 4; i++) {
 			code[i] = calc.generate();
 			}
-			//hier genereed de code//
+
 		
 		while (rounds <= 10 && cont == false) {
 			System.out.println("");
@@ -34,29 +35,21 @@ public class MainGame {
 				System.out.println("Raad de kleur van de"+ " " +(i+1)+ "e" + " "+ "positie.");
 				System.out.println("Kies uit: Rd, Bl, Gr, Gl, Or, La");
 				poging[i] = input.nextLine();
+				
 			//hier checkt het of de input geldig is//
 				while (!poging[i].equals("Rd") && !poging[i].equals("Bl") && !poging[i].equals("Gr") && !poging[i].equals("Gl") && !poging[i].equals("Or") && !poging[i].equals("La")) {
 				System.out.println("Kies asjeblieft een mogelijke kleur van de lijst.");
 				poging[i] = input.nextLine();
-			}//hier checkt het of de input geldig is//
-				//hier is het begin input//
+			}
 				
 				//hier zegt de systeem jouwe input//
-		}		System.out.println("Dit is jouw keuze.");
-				System.out.println("--------------------------");
-				System.out.println(poging[0]+" "+poging[1]+" "+poging[2]+" "+poging[3]);
-				//hier zegt de systeem jouwe input//
+		}		calc.yourInput(poging);
+
 				System.out.println("");
 				//hier checkt het systeem of jij iets goed geraden hebt//
 				for (int i = 0; i < 4; i++) {
-					if (poging[i].equals(code[i])) {
-						System.out.println("Positie "+(i+1)+" is goed");
-					}else if(poging[i].equals(code[0])  || poging[i].equals(code[1])|| poging[i].equals(code[2])|| poging[i].equals(code[3])){
-						System.out.println("Positie "+(i+1)+" is bijna goed");
-					}else {
-						System.out.println("Positie "+(i+1)+" is niet goed");
-					}
-				}//hier checkt het systeem of jij iets goed geraden hebt//
+					calc.check(poging, code, i);
+				}
 				rounds = rounds + 1;
 				//hier checkt het systeem of jij gewonen hebt//
 				if (poging[0].equals(code[0]) && poging[1].equals(code[1]) && poging[2].equals(code[2]) && poging[3].equals(code[3])) {
@@ -64,7 +57,7 @@ public class MainGame {
 					rounds = 10;
 					cont = true;
 				}	
-				if (rounds == 10) {
+				else{
 					cont = true;
 					System.out.println("Je hebt verloren");
 					System.out.println("");
@@ -72,7 +65,7 @@ public class MainGame {
 					System.out.println(code[0]+" "+code[1]+" "+code[2]+" "+code[3]);
 					System.out.println("");
 					System.out.println("Volgende keer beter");
-				}//hier checkt het systeem of jij gewonnen hebt//
+				}
 			}
 		}
 }
